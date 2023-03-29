235be385397cd4fdd82d0c79cb90333cd7f6317f
Gian Marco Pablo Totocayo <gianmarcopablo588@gmail.com>
2023-03-28 23:50:34 -0500

    actualizado

--- js/testing2.js
+++ js/testing2.js
@@ -1,62 +1,133 @@
+function Seguro(marca,year,tipo){
+    this.marca = marca
+    this.year = year
+    this.tipo = tipo
+}
+
+Seguro.prototype.cotizarSeguro = function(){
+    /**
+     *  1 = americado + 1.15
+     *  2 = asittico + 1.05
+     *  3 = europeo + 1.35
+     */
+
+    let cantidad;
+    const base = 2000
+    switch (this.marca) {
+        case "1":
+            cantidad = base * 1.15
+            break
+        case "2":
+            cantidad = base * 1.05
+            break
+        case "3":
+            cantidad = base * 1.35
+            break;
+        default:
+            break;
+    }
+
+    //leer el año
+    const diferencia = new Date().getFullYear() - this.year
+
+    cantidad -= ((diferencia * 3) * cantidad) / 100
+    
+    /**
+     * si el seguro es basico se multiplica por un 30 % mas 
+     * si el seguro es completo se multiplica por un 50% mas
+     */
+    if(this.tipo === "basico"){
+        cantidad *= 1.30 
+    }else{
+        cantidad *= 1.50
+    }
+    return cantidad
+}
+
+//** --------------------------------------------------------------------------  */
 function UI(){}
 
+const ui = new UI
+
 UI.prototype.llenarOpciones = function(){
-    //generamos los años
     const max = new Date().getFullYear() - 3
     const min = max - 20
-    
+
     for(let i = max; i > min; i--){
         const option = document.createElement("option")
-        option.textContent = i
         option.value = i
+        option.textContent = i
         const year = document.querySelector("#year")
-        year.appendChild(option) //insertamos los años
+        year.appendChild(option)
     }
 }
-UI.prototype.mostrarMensaje = function(mensaje,tipo){
+
+UI.prototype.mostraMensaje = function(mensaje,tipo){
     const div = document.createElement("div")
     if(tipo === "error"){
         div.classList.add("error")
     }else{
         div.classList.add("correcto")
     }
-
-    div.classList.add("mensaje","mt-10")
+    div.classList.add("mensaje","mt-5")
     div.textContent = mensaje
     const formulario = document.querySelector("#cotizar-seguro")
-    formulario.insertBefore(div, document.querySelector("#resultado"))
-    setTimeout(()=>{
+    formulario.insertBefore(div,document.querySelector("#resultado"))
+    setTimeout(() => {
         div.remove()
+    }, 3000);
+}
+
+UI.prototype.mostraResultado = function(total,seguro){
+    //crear el resultado
+    const div = document.createElement("div")
+    div.classList.add("mt-10")
+    div.innerHTML = `
+        <p class="header">Tu resumen</p>
+        <p class="font-bold">${total}</p>
+    `
+    const resultadoDiv = document.querySelector("#resultado")
+    const spinner = document.querySelector("#cargando")
+    spinner.style.display = "block"
+    setTimeout(()=>{
+        spinner.style.display = "none"
+        resultadoDiv.appendChild(div)
     },3000)
 }
 
-
-const ui = new UI //instacioamos el ui
-//*-------------------------------------------------------------------- */
-//EVENTLISTENER DE LA INSTANCIA UI
 document.addEventListener("DOMContentLoaded",()=>{
-    ui.llenarOpciones() //llamamos la funcion instanciada de ui
+    ui.llenarOpciones()
 })
 
-enventLisnteners()
-function enventLisnteners(){
+cargarEventListeners()
+function cargarEventListeners(){
     const formulario = document.querySelector("#cotizar-seguro")
     formulario.addEventListener("submit",cotizarSeguro)
 }
 
 function cotizarSeguro(e){
     e.preventDefault()
-
-    //leer la marca seleccioanda
     const marca = document.querySelector("#marca").value
-    //leer el año seleccinado
     const year = document.querySelector("#year").value
-    //leer el tipo de cobertura
-    const tipo = document.querySelector('input[name="tipo"]:checked').value
+    const tipo = document.querySelector("input[name='tipo']:checked").value
 
     if(marca === "" || year === "" || tipo === ""){
-        ui.mostrarMensaje("TODOS LOS CAMPOS SON OBLIGATORIOS","error")
+        ui.mostraMensaje("TODOS LOS CAMPOS SON OBLIGATORIOS","error")
     }else{
-        ui.mostrarMensaje("COTIZANDO...","exito")
+        ui.mostraMensaje("COTIZANDO...","exito")
+
+        const resultado = document.querySelector("#resultado")
+        if(resultado !== null){
+            resultado.remove()
+        }
+        const seguro = new Seguro(marca,year,tipo)
+        const total = seguro.cotizarSeguro()
+
+        ui.mostraResultado(total,seguro)
     }
+
+
 }
+
+
+//** --------------------------------------------------------------------------  */
