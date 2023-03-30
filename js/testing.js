d519d4c49dd6e2166fe1b4cbfe38a260424e19f3
Gian Marco Pablo Totocayo <gianmarcopablo588@gmail.com>
2023-03-29 22:56:40 -0500

    actualizado

--- js/testing.js
+++ js/testing.js
@@ -1,72 +1,108 @@
 
-//*-------------------------------------------------------------------- */
-// PROTOTYPO SEGURO
 function Seguro(marca,year,tipo){
     this.marca = marca
     this.year = year
     this.tipo = tipo
 }
-//*-------------------------------------------------------------------- */
-//PROTOTYPO UI
+
+Seguro.prototype.cotizarSeguro = function(){
+    /**
+     * 1 = americano * 1.15
+     * 2 = asiatico * 1.05
+     * 3= europeo  * 1.35
+    */
+
+    let cantidad;
+    const base = 2000
+    switch (this.marca) {
+        case "1":
+            cantidad = base * 1.15
+            break;
+        case "2":
+            cantidad = base * 1.05
+            break;
+        case "e":
+            cantidad = base * 1.35
+            break
+        default:
+            break;
+    }
+
+
+    const diferencia = new Date().getFullYear() - this.year
+    cantidad = ((diferencia * 3)* cantidad) / 100
+
+    /**
+     * Si el seguro es basico se multiplica por un 30% mas
+     * Si el seguro es completo se multiplica por un 50% mas
+     */
+
+    if(this.tipo === "basico"){
+        cantidad *= 1.30
+    }else{
+        cantidad *= 1.50
+    }
+    console.log(cantidad)
+}
+
 function UI(){}
 
 UI.prototype.llenarOpciones = function(){
-    //generamos los años
     const max = new Date().getFullYear() - 3
     const min = max - 20
-    
     for(let i = max; i > min; i--){
-        const option = document.createElement("option")
-        option.textContent = i
+        const option = document.createElement('option')
         option.value = i
-        const year = document.querySelector("#year")
-        year.appendChild(option) //insertamos los años
+        option.textContent = i
+        const year = document.querySelector('#year')
+        year.appendChild(option)
     }
 }
-UI.prototype.mostrarMensaje = function(mensaje,tipo){
+
+UI.prototype.mostrarMensaje = function(mensaje, tipo){
     const div = document.createElement("div")
     if(tipo === "error"){
         div.classList.add("error")
     }else{
         div.classList.add("correcto")
     }
-
     div.classList.add("mensaje","mt-10")
     div.textContent = mensaje
     const formulario = document.querySelector("#cotizar-seguro")
-    formulario.insertBefore(div, document.querySelector("#resultado"))
     setTimeout(()=>{
         div.remove()
     },3000)
+    const errores = document.querySelectorAll(".error")
+    if(errores.length === 0){
+        formulario.insertBefore(div, document.querySelector("#resultado"))
+    }
 }
 
 
-const ui = new UI //instacioamos el ui
-//*-------------------------------------------------------------------- */
-//EVENTLISTENER DE LA INSTANCIA UI
+const ui = new UI()
+
 document.addEventListener("DOMContentLoaded",()=>{
-    ui.llenarOpciones() //llamamos la funcion instanciada de ui
+    ui.llenarOpciones()
 })
 
-enventLisnteners()
-function enventLisnteners(){
+cargarEventListeners()
+function cargarEventListeners(){
     const formulario = document.querySelector("#cotizar-seguro")
     formulario.addEventListener("submit",cotizarSeguro)
 }
 
 function cotizarSeguro(e){
     e.preventDefault()
-
-    //leer la marca seleccioanda
-    const marca = document.querySelector("#marca").value
-    //leer el año seleccinado
+    const marca  = document.querySelector("#marca").value
     const year = document.querySelector("#year").value
-    //leer el tipo de cobertura
     const tipo = document.querySelector('input[name="tipo"]:checked').value
 
     if(marca === "" || year === "" || tipo === ""){
-        ui.mostrarMensaje("TODOS LOS CAMPOS SON OBLIGATORIOS","error")
+        ui.mostrarMensaje("Todos los campos son obligatorios", "error")
     }else{
-        ui.mostrarMensaje("COTIZANDO...","exito")
+        ui.mostrarMensaje("Cotizando...", "correcto")
+        const seguro = new Seguro(marca,year,tipo)
+        seguro.cotizarSeguro()
     }
 }
+
